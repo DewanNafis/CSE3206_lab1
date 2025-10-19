053f9d4a7c55d439f66db034dd355ddd5b442d99
dewan_nafis <dewannafisiqbal@gmail.com>
2025-10-19 14:45:45 +0600

    Added factorial function in feature-update branch

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Hello, Git and GitHub!" << endl;
+int factorial(int n){
+    return (n <= 1) ? 1 : n * factorial(n - 1);
+}
+int main(){
+    cout << "Factorial of 5: " << factorial(5) << endl;
     return 0;
 }
